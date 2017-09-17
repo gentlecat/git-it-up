54c0887378fd87e5671bfdadf14d79b134b77da8
Roman Tsukanov <roman@tsukanov.me>
2017-09-17 21:22:04 +0900

    Add support for syncing multiple repos with the same script
    Also improve logging a bit and some other minor things.

--- git.rb
+++ git.rb
@@ -1,29 +1,58 @@
-# Utility functions for working with a git repository
+# Utility functions for working with Git repositories
+require 'logger'
+
+
 module Git
   module_function
 
   UP_TO_DATE_MSG = 'Your branch is up-to-date'.freeze
   BEHIND_MSG = 'Your branch is behind'.freeze
 
+  # TODO: Consolidate logger definitions in one place (see update.rb file)
+  @@log = Logger.new($stdout)
+  @@log.level = Logger::INFO
+
+  def update!(repo_dir)
+    Dir.chdir(repo_dir) do
+      Git.check_remote
+      if Git.outdated?
+        @@log.info "Repository `#{repo_dir}` is outdated. Updating..."
+        Git.pull
+        @@log.info "Updated `#{repo_dir}`."
+      else
+        @@log.info "Repository `#{repo_dir}` is up to date."
+      end
+    end
+  end
+
   def pull
     run_cmd('pull')
   end
 
-  def update_remote
+  def check_remote
     run_cmd('remote update')
+  end
+
+  # Checks if a directory is a Git repository.
+  # One caveat is that it will consider `dir` as a repository even if it's
+  # just a subdirectory of an existing repository.
+  def is_repo?(dir)
+    is_repo = false
+    Dir.chdir(dir) do
+      is_repo = `git rev-parse --is-inside-work-tree`.chomp == 'true'
+    end
+    is_repo
   end
 
   def outdated?
     status_msg = status
     status_rows = status_msg.split(/\n/)
     if status_rows.length < 2
-      puts "Cannot parse unexpected status message:\n#{status_msg}"
-      exit(false)
+      @@log.fail "Cannot parse unexpected status message:\n#{status_msg}"
     end
     return false if status_rows[1].start_with?(UP_TO_DATE_MSG)
     return true if status_rows[1].start_with?(BEHIND_MSG)
-    puts "Unexpected status message:\n#{status_msg}"
-    exit(false)
+    @@log.fail "Unexpected status message:\n#{status_msg}"
   end
 
   # Get status of the repository
@@ -38,8 +67,7 @@ module Git
     output = `#{cmd}`.chomp
     exit_status = $?.exitstatus
     if exit_status > 1
-      puts "Command `#{cmd} has failed. Exit status: #{exit_status}`"
-      exit(false)
+      @@log.fail "Command `#{cmd} has failed. Exit status: #{exit_status}`"
     end
     output
   end
--- update.rb
+++ update.rb
@@ -1,14 +1,42 @@
 require 'logger'
+require 'optparse'
 require_relative 'git'
 
-REPO_DIR = ARGV[0].freeze
-Dir.chdir(REPO_DIR)
+SLEEP_DURATION = 20 # seconds
 
-Git.update_remote
-if Git.outdated?
-  print 'Repository is outdated. Updating...'
-  Git.pull
-  print " Done.\n"
-else
-  print "Repository is up to date.\n"
+$stdout.sync = true
+log = Logger.new($stdout)
+# TODO: Make logging level customizable using a command line argument
+log.level = Logger::WARN
+
+list_file = nil
+OptionParser.new do |parser|
+  parser.on('-l', '--list=MANDATORY' 'File with a repository list') do |list|
+    list_file = list
+  end
+end.parse!
+
+# Reading and validating the repository list
+unless File.file? list_file
+  puts "Unable to read file `#{list_file}`!"
+  exit false
+end
+dirs = File.read(list_file).split(/\n/)
+if dirs.length < 1 
+  puts 'Repository list file is empty!'
+end
+dirs.each do |dir|
+  log.debug "Checking if #{dir} is a git repository"
+  unless Git.is_repo?(dir)
+    puts "Directory `#{dir}` is not a git repository!"
+    exit false
+  end
+end
+
+# The main update loop
+loop do
+  dirs.each do |repo|
+    Git.update! repo
+  end
+  sleep SLEEP_DURATION
 end
